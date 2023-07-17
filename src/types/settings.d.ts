fdce12d29c1f0980db04d43c49238ee837534af9
Karma <296045247@qq.com>
2023-07-17 19:25:50 +0800

    chore: 添加全局types

---
+++ src/types/settings.d.ts
@@ -0,0 +1,11 @@
+declare namespace Settings {
+  interface Option {
+    theme: string
+    api_url: string
+    api_key: string
+    model: string
+    role_nick_name: string
+    role_remarks: string
+    role_directive: string
+  }
+}
